55cf629ee53da25d6ebcb3cf0dc1172be9edf991
pablodezutter <dezutterpablo@gmail.com>
2019-06-17 17:02:37 +0200

    one missing

--- ReactByEd/projectreact/src/App.js
+++ ReactByEd/projectreact/src/App.js
@@ -3,19 +3,24 @@ import Tweet from './Tweet';
 
 function App(){
 
-const [isRed, setRed]= useState(false);
-const [count, setCount]= useState(0);
+const [users, setUsers] = useState ([
 
-const increment= () =>{
-    setCount(count + 1);
-    setRed (!isRed);
-}
+{name: "John Snow", message:"coucoo"},
+{name: "Fred", message: "I am awesome"},
+{name: "Dirk", message: "It's my birthday"}
+
+]
+
+
+
+);
 
 return(
   <div className='app'>
-  <h1 className = {isRed ? 'red' : ""}>Change my Color</h1>
-  <button onClick = {increment}>Increment</button>
-  <h1>{count}</h1>
+    {users.map(user =>(
+        <Tweet name={user.name} message={user.message} />
+
+     ) )}
   </div>
 
 
