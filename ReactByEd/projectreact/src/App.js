1707e62812e3471cccb88b8d1ee369c6e220c099
pablodezutter <dezutterpablo@gmail.com>
2019-06-17 16:47:04 +0200

    const, functions

--- ReactByEd/projectreact/src/App.js
+++ ReactByEd/projectreact/src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Tweet from './Tweet';
 
 function App(){
+
+const [isRed, setRed]= useState(false);
+const [count, setCount]= useState(0);
+
+const increment= () =>{
+    setCount(count + 1);
+    setRed (!isRed);
+}
+
 return(
   <div className='app'>
-        <Tweet/>
-    <Tweet/>
-    <Tweet/>
-    <Tweet/>
+  <h1 className = {isRed ? 'red' : ""}>Change my Color</h1>
+  <button onClick = {increment}>Increment</button>
+  <h1>{count}</h1>
   </div>
 
 
